b5f83ddf79231138b99a087074dd43f312ed4017
peu77 <emilebert@icloud.com>
2021-09-25 23:31:31 +0200

    add animation to test

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,6 +4,26 @@
     import Button from "../components/button.svelte"
     import CreateTest from "../components/modal/list/createTest.svelte"
     import Input from "../components/input.svelte"
+    import { quintOut } from 'svelte/easing';
+    import { crossfade } from 'svelte/transition';
+    import {flip} from 'svelte/animate';
+    const [send, receive] = crossfade({
+        duration: d => Math.sqrt(d * 200),
+
+        fallback(node, params) {
+            const style = getComputedStyle(node);
+            const transform = style.transform === 'none' ? '' : style.transform;
+
+            return {
+                duration: 600,
+                easing: quintOut,
+                css: t => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`
+            };
+        }
+    });
 
     let search = ""
 </script>
@@ -14,19 +34,24 @@
 </div>
 
 <div id="tests">
-    {#each $tests.filter(test => test.name.toLowerCase().includes(search.toLowerCase()) || search === "") as test}
-        <Test test={test}/>
-        {/each}
+    {#each $tests.filter(test => test.name.toLowerCase().includes(search.toLowerCase()) || search === "") as test (test.uuid)}
+        <div in:receive="{{key: test.uuid}}"
+             animate:flip
+       >
+            <Test test={test}/>
+        </div>
+
+    {/each}
 </div>
 
 <style>
-    #controls{
+    #controls {
         display: flex;
         margin-bottom: 16px;
         gap: 16px;
     }
 
-    #tests{
+    #tests {
         grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
         display: grid;
         grid-gap: 16px;
